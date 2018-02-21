a57eb8b292e801485a6ef40a38cccdbd1c627107
Senez Thomas <senezth@gmail.com>
2018-02-21 14:50:55 +0100

    get calls by group

--- mongodb/import.js
+++ mongodb/import.js
@@ -36,6 +36,7 @@ var insertCalls = function(db, callback) {
         });
 }
 
+// Function for first request
 var searchCountInRadius = (db, lat, lng, radius, callback) => {
     var calls = db.collection("calls");
     radius = metersToRadians(radius);
@@ -62,30 +63,69 @@ var metersToRadians = (radius) => {
     return radius / EARTH_RADIUS_IN_METERS;
 }
 
-MongoClient.connect(mongoUrl, (err, db) => {
-    insertCalls(db, result => {
-        var calls = db.collection("calls")
-        console.log(`${result.insertedCount} calls inserted`);
-        calls.createIndex({location : "2dsphere"}, (err, result) => {
-            if (err){
-                console.log(err);
-            } else {
-                console.log("2dsphere index added");
+
+// Function for 2nd request
+var sortByCategory = (db, callback) => {
+    var calls = db.collection("calls");
+
+    calls.find({
+        $text : {
+            $search : 
+                "\"EMS:\""
+        }
+    }).count((err, result) => {
+        var EMSnumber = result;
+        calls.find({
+            $text : {
+                $search : 
+                    "\"Traffic:\""
             }
-            calls.createIndex({title : "text"}, (err, result) => {
+        }).count((err, result) => {
+            var trafficNumber = result;
+            calls.find({
+                $text : {
+                    $search : 
+                        "\"Fire:\""
+                }
+            }).count((err, result) => {
+                var fireNumber = result;
+                callback(EMSnumber, trafficNumber, fireNumber);
+            })
+        })
+    })
+}
+
+MongoClient.connect(mongoUrl, (err, db) => {
+    // Whole process for the 4 requests
+    db.collection("calls").drop((err, del) => {
+        insertCalls(db, result => {
+            var calls = db.collection("calls")
+            console.log(`${result.insertedCount} calls inserted`);
+            calls.createIndex({location : "2dsphere"}, (err, result) => {
                 if (err){
                     console.log(err);
                 } else {
-                    console.log("text index added");
+                    console.log("2dsphere index added");
                 }
-                console.log("searching for calls in a 500m radius to Lansdale, PA, USA...");
-                searchCountInRadius(db, 40.241493, -75.283783, 500, (result) => {
-                    console.log(result);
-                    db.close();
+                calls.createIndex({title : "text"}, (err, result) => {
+                    if (err){
+                        console.log(err);
+                    } else {
+                        console.log("text index added");
+                    }
+                    console.log("searching for calls in a 500m radius to Lansdale, PA, USA...");
+                    searchCountInRadius(db, 40.241493, -75.283783, 500, (result) => {
+                        console.log(result);
+                        console.log("searching calls grouped by type...");
+                        sortByCategory(db, (ems, traffic, fire) => {
+                            console.log("EMS calls : " + ems);
+                            console.log("Traffic calls : " + traffic);
+                            console.log("Fire calls : " + fire);
+                            db.close();
+                        })
+                    })
                 })
-            })
+            });
         });
     });
 });
-
-// db.calls.find( { location : { $geoWithin : { $centerSphere :  [ [ -75.283783, 40.241493 ] , 500/6378100 ] } } } ).count()
